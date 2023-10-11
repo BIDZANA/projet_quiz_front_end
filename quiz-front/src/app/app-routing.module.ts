ec442a52b066810f6953d408446d1ac685788f1b
MalickaHoumgbo <malickamaeva.houmgbombakop@epfedu.fr>
2023-10-10 16:15:06 -0800

    Page connexion

--- quiz-front/src/app/app-routing.module.ts
+++ quiz-front/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { ThemesListComponent } from './themes-list/themes-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AccueilComponent},
+  { path: 'accueil', component: AccueilComponent},
+  { path: 'inscription', component: InscriptionComponent },
+  { path: 'connexion', component: ConnexionComponent},
+  { path: 'themes-list', component: ThemesListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- quiz-front/src/app/app.module.ts
+++ quiz-front/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { ThemesListComponent } from './themes-list/themes-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InscriptionComponent,
+    AccueilComponent,
+    ConnexionComponent,
+    ThemesListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
