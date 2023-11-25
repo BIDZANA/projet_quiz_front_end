7d126214f80d418282a32f0fe8b50e0d4e0420d6
MalickaHoumgbo <123568770+MalickaHoumgbo@users.noreply.github.com>
2023-11-25 11:16:48 -0800

    Model Game

--- src/app/models/Game/game.ts
+++ src/app/models/Game/game.ts
@@ -1,2 +1,13 @@
 export class Game {
+    id_game: number;
+    date: Date;
+    score: number;
+    id_quiz: number;
+  
+    constructor(id_game: number, date: Date, score: number, id_quiz: number) {
+      this.id_game = id_game;
+      this.date = date;
+      this.score = score;
+      this.id_quiz = id_quiz;
+    }
 }
